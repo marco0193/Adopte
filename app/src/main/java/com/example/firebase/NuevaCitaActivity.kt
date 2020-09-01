cd6642deda008c7a6aa60a56c69dba942ad151de
Marco Andres <marco19398@gmail.com>
2020-08-31 22:28:56 -0600

    Validación de todos los campos

--- app/src/main/java/com/example/firebase/NuevaCitaActivity.kt
+++ app/src/main/java/com/example/firebase/NuevaCitaActivity.kt
@@ -40,9 +40,9 @@ class NuevaCitaActivity : AppCompatActivity() {
                 //obtenemos los minutos
                 calendar.set(Calendar.MINUTE, minute)
 
+                //Mostramos la hora en el textview
                 horaCita.text = SimpleDateFormat("HH:mm").format(calendar.time).toString()
             }
-
             TimePickerDialog(this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show()
         }
 
@@ -66,35 +66,64 @@ class NuevaCitaActivity : AppCompatActivity() {
         val database = AppDataBase.getDatabase(this)
 
         bGuardar.setOnClickListener{
+            //Comprobamos que el campo del nombre del cliente tenga valores
             if(nombreCliente.text.isEmpty()){
-                Toast.makeText(this, "Ingresar un nombre de cliente valido", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Ingresar un nombre de cliente", Toast.LENGTH_SHORT).show()
             }else{
-
-                val cliente = nombreCliente.text.toString()
-                val fecha = fechaCita.text.toString()
-                val hora = horaCita.text.toString()
-                val telefono = telefono.text.toString()
-                val tipo = tipo.text.toString()
-                val precio = precio.text.toString().toInt()
-                val descripcion = descripcion.text.toString()
-
-                val cita = Cita(cliente, fecha, hora, telefono, tipo, precio, descripcion)
-
-                if (idCita != null){
-                    CoroutineScope(Dispatchers.IO).launch {
-                        cita.idCita = idCita
-
-                        database.citas().update(cita)
-
-                        this@NuevaCitaActivity.finish()
-                    }
+                //Comprobamos que el campo fecha tenga valores
+                if(fechaCita.text.isEmpty()){
+                    Toast.makeText(this, "Ingresar una fecha", Toast.LENGTH_SHORT).show()
                 }else{
-                    CoroutineScope(Dispatchers.IO).launch {
-                        database.citas().insertAll(cita)
-                        this@NuevaCitaActivity.finish()
+                    //Comprobamos que el campo hora tenga valores
+                    if(horaCita.text.isEmpty()){
+                        Toast.makeText(this, "Ingresar una hora", Toast.LENGTH_SHORT).show()
+                    }else{
+                        //Comprobamos que el campo telefono tenga valores
+                        if(telefono.text.isEmpty()){
+                            Toast.makeText(this, "Ingresar una telefono", Toast.LENGTH_SHORT).show()
+                        }else{
+                            //Comprobamos que el campo tipo de cita tenga valores
+                            if(tipo.text.isEmpty()){
+                                Toast.makeText(this, "Ingresar el tipo de consulta", Toast.LENGTH_SHORT).show()
+                            }else{
+                                //Comprobamos que el campo precio tenga valores
+                                if(precio.text.isEmpty()){
+                                    Toast.makeText(this, "Ingresar el costo de la consulta", Toast.LENGTH_SHORT).show()
+                                }else{
+                                    //Ingrese una descripción
+                                    if(descripcion.text.isEmpty()){
+                                        Toast.makeText(this, "Ingresa una descripción de la consulta", Toast.LENGTH_SHORT).show()
+                                    }else{
+                                        val cliente = nombreCliente.text.toString()
+                                        val fecha = fechaCita.text.toString()
+                                        val hora = horaCita.text.toString()
+                                        val telefono = telefono.text.toString()
+                                        val tipo = tipo.text.toString()
+                                        val precio = precio.text.toString().toInt()
+                                        val descripcion = descripcion.text.toString()
+
+                                        val cita = Cita(cliente, fecha, hora, telefono, tipo, precio, descripcion)
+
+                                        if (idCita != null){
+                                            CoroutineScope(Dispatchers.IO).launch {
+                                                cita.idCita = idCita
+
+                                                database.citas().update(cita)
+
+                                                this@NuevaCitaActivity.finish()
+                                            }
+                                        }else{
+                                            CoroutineScope(Dispatchers.IO).launch {
+                                                database.citas().insertAll(cita)
+                                                this@NuevaCitaActivity.finish()
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
-
             }
 
 
